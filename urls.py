c45825082ee06a6833f90a4b8853d3a6f88ba92e
Bhodikorn <112610961+Bhodikorn@users.noreply.github.com>
2023-09-13 15:45:39 +0700

    V1.2

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # Import the 'include' function
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('current-time/', include('timeapp.urls')),  # Include the 'timeapp' URLs
-]
--- views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from datetime import datetime
-
-def current_time(request):
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    return render(request, 'timeapp/current_time.html', {'current_time': current_time})
