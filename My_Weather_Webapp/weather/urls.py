0324dc206d31dc9d30bbfb739367b1b38a76c114
Bhodikorn <112610961+Bhodikorn@users.noreply.github.com>
2023-10-11 18:09:49 +0700

    gooooooooo

--- My_Weather_Webapp/weather/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.index, name = 'home'),
-    path('delete/<city_name>/', views.delete_city, name='delete_city'),
-]
