2be8794e1f0b2cf4339ca18e269d99f6169509cc
kittipatw <144888365+kittipatw@users.noreply.github.com>
2023-11-21 22:10:57 +0700

    Update settings.py

--- main_website/rain_web/settings.py
+++ main_website/rain_web/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'dashboard'
 ]
 
@@ -53,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
 
 ROOT_URLCONF = 'rain_web.urls'
